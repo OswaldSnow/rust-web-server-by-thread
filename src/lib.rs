2ea47fdd044f3ed3188f108013d670fca85cd5e6
OswaldSnow-ubt <OswaldSnow88@gmail.com>
2025-03-11 20:51:25 +0800

    0311

--- src/lib.rs
+++ src/lib.rs
@@ -1,67 +1,35 @@
+// src/lib.rs
 use std::{
-    fs,
-    io::{BufRead, BufReader, Write},
-    net::TcpStream,
     sync::{mpsc, Arc, Mutex},
-    thread::{self},
-    time::Duration,
+    thread,
 };
 
-#[allow(unused)]
-struct Worker {
-    id: usize,
-    thread: Option<thread::JoinHandle<()>>,
-}
-
-impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(move || loop {
-            println!("thread {id} is waiting");
-
-            let message = receiver.lock().unwrap().recv();
-
-            match message {
-                Ok(job) => job(),
-                Err(_) => break,
-            };
-        });
-
-        // 此种方式无法实现多线程处理请求 会卡住
-        // let thread = thread::spawn(move || {
-        //     while let Ok(job) = receiver.lock().unwrap().recv() {
-        //         println!("Worker {id} got a job; executing.");
-
-        //         job();
-        //     }
-        // });
-
-        Worker {
-            id,
-            thread: Some(thread),
-        }
-    }
-}
-
-type Job = Box<dyn FnOnce() + Send + 'static>;
-
-#[allow(unused)]
 pub struct ThreadPool {
     workers: Vec<Worker>,
     sender: Option<mpsc::Sender<Job>>,
 }
 
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
 impl ThreadPool {
-    pub fn new(size: usize) -> Self {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
         let (sender, receiver) = mpsc::channel();
 
-        let mut workers = Vec::with_capacity(4);
-
         let receiver = Arc::new(Mutex::new(receiver));
 
-        for id in 0..4 {
-            workers.push(Worker::new(id, receiver.clone()));
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
         ThreadPool {
@@ -82,7 +50,11 @@ impl ThreadPool {
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        drop(self.sender.take());
+
         for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
             if let Some(thread) = worker.thread.take() {
                 thread.join().unwrap();
             }
@@ -90,24 +62,32 @@ impl Drop for ThreadPool {
     }
 }
 
-pub fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
 
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv();
 
-    let (status_line, res_file_name) = match &request_line[..] {
-        line if line.eq_ignore_ascii_case("GET / HTTP/1.1") => ("HTTP/1.1 200 OK", "hello.html"),
-        line if line.eq_ignore_ascii_case("GET /sleep HTTP/1.1") => {
-            thread::sleep(Duration::from_secs(5));
-            ("HTTP/1.1 200 OK", "sleep.html")
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
+            }
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
         }
-        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
-    };
-
-    let contents = fs::read_to_string(res_file_name).unwrap();
-    let len = contents.len();
-
-    let response_info = format!("{status_line}\r\nContent-length: {len}\r\n\r\n\n{contents}");
-
-    stream.write_all(response_info.as_bytes()).unwrap();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,52 @@
+// src/main.rs
+use rust_web_server_by_thread::ThreadPool;
+use std::fs;
+use std::io::prelude::*;
 use std::net::TcpListener;
-
-use rust_web_server_by_thread::{handle_connection, ThreadPool};
+use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    // 初始化线程池
     let pool = ThreadPool::new(4);
 
     for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
-        // 单线程处理请求
-        // handle_connection(stream);
-
-        // 每一个请求使用单独的线程进行处理
-        // thread::spawn(|| handle_connection(stream));
-
-        // 使用线程池处理请求
-        pool.execute(|| handle_connection(stream));
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
+
+    println!("Shutting down.");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
